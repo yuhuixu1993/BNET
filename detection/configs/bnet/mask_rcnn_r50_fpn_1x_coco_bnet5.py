202b905a45d3568dcbcb3ea508e0d16f4e9ce425
Yuhui Xu <yuhuixu1993@126.com>
2020-11-28 11:24:20 -0500

    Create mask_rcnn_r50_fpn_1x_coco_bnet5.py

---
+++ detection/configs/bnet/mask_rcnn_r50_fpn_1x_coco_bnet5.py
@@ -0,0 +1,5 @@
+_base_ = [
+    '../_base_/models/mask_rcnn_r50_fpn_bnet5.py',
+    '../_base_/datasets/coco_instance.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
